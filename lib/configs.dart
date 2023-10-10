8f923d1dbf2b4a50f919aa892b4bd66f56fed721
Ihor Nazarov <ihor.nazarov@intive.com>
2023-10-10 16:25:11 +0200

    v 1.0.0 (2)

--- lib/configs.dart
+++ lib/configs.dart
@@ -11,6 +11,24 @@ abstract final class AppInsets {
 
 abstract final class Constants {
   static const BorderRadius standardRadius = BorderRadius.all(Radius.circular(10));
+
+  static const String appInfo = '''This is a counting app that allows you to: 
+          
+* check if a number is prime (a number that is only divisible by 1 and itself) 
+          
+* compute the “nth” prime (this is actually doing an API request to Wolfram Alpha, a scientific computing API)
+
+* ask for a fact about a number (which actually makes a request to an external API service for the fact)
+
+* save and share your favorite facts.
+
+The app is inspired by the series of Point Free episodes and was born as a result of my first steps of learning Flutter. 
+''';
+
+  static const String pointFreeUrl = 'https://pointfree.co';
+  static const String wolframAlphaUrl = 'https://wolframalpha.com';
+  static const String numbersApiUrl = 'http://numbersapi.com';
+  static const String flutterUrl = 'https://flutter.dev';
 }
 
 abstract final class Styles {
@@ -19,5 +37,10 @@ abstract final class Styles {
     textStyle: const TextStyle(
       fontSize: 20,
     ),
+  );
+
+  static TextStyle linkTextStyle = const TextStyle(
+    decoration: TextDecoration.underline,
+    color: Colors.blue,
   );
 }
--- lib/info/info_page.dart
+++ lib/info/info_page.dart
@@ -1,4 +1,7 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:humble_counter/configs.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class InfoPage extends StatelessWidget {
   const InfoPage({super.key});
@@ -7,8 +10,17 @@ class InfoPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: _buildAppBar(context),
-      body: const Center(
-        child: Text('Content'),
+      body: Padding(
+        padding: AppInsets.standard,
+        child: Column(
+          children: [
+            const Text(Constants.appInfo),
+            _buildLink('Point Free', Constants.pointFreeUrl),
+            _buildLink('WolframAlpha', Constants.wolframAlphaUrl),
+            _buildLink('Numbers API', Constants.numbersApiUrl),
+            _buildLink('Flutter', Constants.flutterUrl),
+          ],
+        ),
       ),
     );
   }
@@ -18,5 +30,30 @@ class InfoPage extends StatelessWidget {
       backgroundColor: Theme.of(context).colorScheme.background,
       title: const Text('Info'),
     );
+  }
+
+  Widget _buildLink(String title, String url) {
+    return InkWell(
+      onTap: () => onLinkTap(url),
+      child: Text(
+        title,
+        style: Styles.linkTextStyle,
+      ),
+    );
+  }
+
+  void onLinkTap(String url) async {
+    try {
+      final res = await launchUrl(Uri.parse(url));
+      if (!res) {
+        if (kDebugMode) {
+          print('Failed to open url $url');
+        }
+      }
+    } catch (error) {
+      if (kDebugMode) {
+        print(error);
+      }
+    }
   }
 }
